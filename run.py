89f4b2274ae47317211007404aa23c2f337fde6f
firoyang <firoyang@tencent.com>
2024-10-03 00:10:14 +0800

    bugfix

--- run.py
+++ run.py
@@ -4,8 +4,9 @@ import shutil
 # 当前脚本的目录
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
+source_name = '3DPrintedMetricV3.xml'
 # 源文件的路径
-source_path = os.path.join(script_dir, '3DPrintedMetricV3.xml')
+source_path = os.path.join(script_dir, source_name)
 
 # 目标目录的主路径
 main_target_dir = os.path.join(os.getenv('LOCALAPPDATA'), 'Autodesk', 'webdeploy', 'Production')
@@ -17,7 +18,7 @@ for dir_name in os.listdir(main_target_dir):
     # 检查这个路径是否存在
     if os.path.exists(full_target_dir):
         # 目标文件的路径
-        target_path = os.path.join(full_target_dir, 'a.xml')
+        target_path = os.path.join(full_target_dir, source_name)
         # 执行拷贝
         shutil.copyfile(source_path, target_path)
         print(f'Copied to {target_path}')
